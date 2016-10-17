9bc0d0b8f7d377149f8ffcad08e12503f01956d1
Sergey Simonenko <ssa.simonenko@gmail.com>
2016-10-17 23:29:06 +0300

    Decrase amount of cells to fix perfomance issue. Update GOL algoritm.

--- src/Config.hs
+++ src/Config.hs
@@ -1,10 +1,9 @@
 module Config where
 
-cellSize = (5,   5)   :: (Int, Int)
+cellSize = (10,  10)  :: (Int, Int)
 plotSize = (500, 500) :: (Int, Int)
 
 cellPadding = 0.5 :: Float
-seedDivisor = 100 :: Int
 
 -- ToDo make this comfortable for continues search!
 neighbours x y = [(x - 1, y - 1), (x, y - 1), (x + 1, y - 1),
--- src/Model.hs
+++ src/Model.hs
@@ -19,7 +19,7 @@ makeField :: MonadRandom m => m Field
 makeField = fmap constructor $ sequence $ generateCells xCount yCount where
     xCount = div (fst plotSize) $ fst cellSize
     yCount = div (snd plotSize) $ snd cellSize
-    constructor cells = Field cells (xCount + 1) (yCount + 1)
+    constructor cells = Field cells xCount yCount
 
 generateCells :: MonadRandom m => Int -> Int -> [m Cell]
 generateCells xCount yCount = do
@@ -29,25 +29,28 @@ generateCells xCount yCount = do
 
 makeCell :: MonadRandom m => Int -> Int -> m Cell
 makeCell x y = fmap (cellConstructor . aliveFunction) getRandom where
-    aliveFunction seed = mod seed seedDivisor
-    cellConstructor = Cell x y
+    aliveFunction seed = mod seed 2
+    cellConstructor    = Cell x y
 
 update :: Field -> Float -> Field
 update (Field cellList xMaxVal yMaxVal) _ = Field (map modifyCell cellList) xMaxVal yMaxVal where
---     modifyCell (Cell x y alive) | alive == 1 = Cell x y 0
---                                 | otherwise  = Cell x y 1
     --
-    modifyCell (Cell x y alive) | alive == 1 && (liveCount  < 2)                   = Cell x y 0
-                                | alive == 1 && (liveCount == 2 || liveCount == 3) = Cell x y 1
-                                | alive == 1 && (liveCount  > 3)                   = Cell x y 0
-                                | alive == 0 && (liveCount == 3)                   = Cell x y 1
-                                | otherwise                                        = Cell x y 0
-                                where
-                                    liveCount = liveCellCounter (neighbours x y) cellList 0
+    modifyCell (Cell x y alive) = updateCell x y alive (liveCellCounter (neighbours x y) cellList 0)
+    --
+    updateCell x y alive liveCount | alive == 1 && (liveCount  < 2)                   = Cell x y 0
+                                   | alive == 1 && (liveCount == 2 || liveCount == 3) = Cell x y 1
+                                   | alive == 1 && (liveCount  > 3)                   = Cell x y 0
+                                   | alive == 0 && (liveCount == 3)                   = Cell x y 1
+                                   | otherwise                                        = Cell x y 0
     --
     liveCellCounter [] _  sum = sum
     liveCellCounter _  [] sum = sum
-    liveCellCounter ns@((x, y) : xs) cs@(c : oc) sum | mod x xMaxVal == xNumber c &&
-                                                       mod y yMaxVal == yNumber c = liveCellCounter xs oc (sum + 1)
-                                                     | otherwise                  = liveCellCounter ns oc  sum
+    liveCellCounter ns@((x, y) : xs) cs@(c : oc) sum | normalize x xMaxVal == xNumber c &&
+                                                       normalize y yMaxVal == yNumber c = liveCellCounter xs oc (sum + isAlive c)
+                                                     | otherwise                        = liveCellCounter ns oc  sum
+                                                     where
+                                                        normalize coord max | coord >= 0 = mod coord max
+                                                                            | otherwise  = mod coord max + 1
+
+
 
